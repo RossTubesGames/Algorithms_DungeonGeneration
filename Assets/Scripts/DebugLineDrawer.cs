e4265a574f7fc71c23548e061d62cccfa3bf9466
RossTubesGames <jordanross1414@gmail.com>
2026-03-02 14:51:06 +0100

    added logic for drawing a line in the script

--- Assets/Scripts/DebugLineDrawer.cs
+++ Assets/Scripts/DebugLineDrawer.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class DebugLineDrawer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    // Start point of the line in the unity world space
+    [SerializeField]
+    private Vector3 start = new Vector3(0f, 0f, 0f);
 
-    // Update is called once per frame
-    void Update()
+    //end point of the line in world space
+    [SerializeField]
+    private Vector3 end = new Vector3(5f, 0f, 0f); //moving forward
+
+    //function to draw debug visual
+    private void OnDrawGizmos()
     {
-        
+        Gizmos.color = Color.yellow;
+
+        // Draw a line from start position to end position
+        Gizmos.DrawLine(start, end);
+
+        // Draw small spheres at both ends so we can see the points
+        Gizmos.DrawSphere(start, 0.1f);
+        Gizmos.DrawSphere(end, 0.1f);
     }
 }
